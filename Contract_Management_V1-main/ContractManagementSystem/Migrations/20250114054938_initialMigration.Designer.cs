a54354aca295e83b6c554df12b9efe956f067495
effiecancode <faitheffie25@gmail.com>
2025-01-14 11:56:42 +0300

    Fix: Docker config

--- Contract_Management_V1-main/ContractManagementSystem/Migrations/20250114054938_initialMigration.Designer.cs
+++ Contract_Management_V1-main/ContractManagementSystem/Migrations/20250114054938_initialMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContractManagementSystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250113081401_initial")]
-    partial class initial
+    [Migration("20250114054938_initialMigration")]
+    partial class initialMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
